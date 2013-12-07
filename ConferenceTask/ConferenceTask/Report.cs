f2ba013cda0ab05bbedc5f9a4b7ada6f8bb43d17
alexanderkavokin <alexanderkavokin@154ce00c-0b6b-ab9b-5dcc-1229dc46d384>
2013-12-07 19:22:25 +0000

    Добавил классы Раписания и Доклада

---
+++ ConferenceTask/ConferenceTask/Report.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConferenceTask
+{
+    public class Report
+    {
+        public Report()
+        {
+            Name = GenerateName();
+        }
+
+        //имя доклада. Генирируется Великим Алгоритмом
+        public string Name { get; set; }
+        //номер секции, в которой находится доклад
+        public int SectionNumber { get; set; }
+        //порядковый номер доклада в секции
+        public int NumberInSection { get; set; }
+        //общий порядковый номер доклада
+        public int Number { get; set; }
+
+        private const int NameMaxLength = 10;
+        private const int NameMinLength = 3;
+
+        /// <summary>
+        /// Великий Алгоритм, создающий имя!
+        /// </summary>
+        /// <returns>
+        /// имя объекту! Андрей, если в будущем буду проблемы с выбором имени ребенка, обращайтесь
+        /// </returns>
+        public string GenerateName()
+        {
+            var random = new Random();
+            var length = random.Next(NameMinLength, NameMaxLength) + 1;
+            var name = "";
+            for (var letter = 0; letter < length; letter++)
+            {
+                var ch = (char)('а' + random.Next(32));
+                name += ch;
+            }
+            return name;
+        }
+    }
+}
---
+++ ConferenceTask/ConferenceTask/Shedule.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ConferenceTask
+{
+    public class Shedule
+    {
+        //количество секций
+        public const int Sections = 3;
+        //количество докладов в одной секции
+        public const int ReportsCountInSection = 10;
+        //общее количество докладов
+        public const int ReportsCount = Sections*ReportsCountInSection;
+
+        //список докладов
+        public List<Report> Reports { get; set; }
+    }
+}
