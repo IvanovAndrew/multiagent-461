ace25610658d2090e916675dbf9727bdf6adcea1
alexanderkavokin <alexanderkavokin@154ce00c-0b6b-ab9b-5dcc-1229dc46d384>
2013-12-11 16:22:45 +0000

    формочка

--- ConferenceTask/ConferenceTask/Form1.cs
+++ ConferenceTask/ConferenceTask/Form1.cs
@@ -10,10 +10,52 @@ namespace ConferenceTask
             Generator.GenerateMatrix();
         }
 
-        private void button1_Click(object sender, System.EventArgs e)
+        private void FillTable(Shedule shedule)
         {
+            foreach (var report in shedule.Reports)
+            {
+                var control = new Label {Text = string.Format("{0}, id = {1}", report.Name, report.Id)};
+                sheduleTable.Controls.Add(control, report.SectionNumber + 1, report.NumberInSection + 1);
+            }
+
+            for (int row = 1; row < 11; row++)
+            {
+                var control = new Label {Text = string.Format("{0}:00 - {1}:00", row + 9, row + 10)};
+                sheduleTable.Controls.Add(control, 0, row);
+            }
+            for (int column = 1; column < 4; column++)
+            {
+                var control = new Label {Text = string.Format("Секция №{0}", column + 1)};
+                sheduleTable.Controls.Add(control, column, 0);
+            }
+        }
+
+        private void button1_Click_1(object sender, System.EventArgs e)
+        {
+//            TextBox textbox = new TextBox();
+//            textbox.Text = "text";
+//            sheduleTable.Controls.Add(new TextBox());
+//            sheduleTable.SetCellPosition(textbox,new TableLayoutPanelCellPosition(2,2) );            
+//            sheduleTable.SetCellPosition(new Control("bla-bla"), new TableLayoutPanelCellPosition(1,1));
+
             var dispatcher = new Dispatcher(Generator.ReadMatrixFromFile());
-            dispatcher.CreateShedule();
+
+            FillTable(dispatcher.CreateShedule());
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void sheduleTable_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
