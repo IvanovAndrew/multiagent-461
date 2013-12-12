c61cbd9922f437c9d262828ee37fe3ecddfe05dd
alexanderkavokin <alexanderkavokin@154ce00c-0b6b-ab9b-5dcc-1229dc46d384>
2013-12-12 19:19:50 +0000

    изменения в форме

--- ConferenceTask/ConferenceTask/Form1.Designer.cs
+++ ConferenceTask/ConferenceTask/Form1.Designer.cs
@@ -109,7 +109,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(425, 56);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Нажми меня нежно!";
+            this.button1.Text = "Составить расписание";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
